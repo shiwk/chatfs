a15c96a2e6d851f9357c96bd1b71513cb2d184f3
shiwk <shiwktju@gmail.com>
2024-01-02 23:57:17 +0800

    readdir impl

--- include/chatfs.h
+++ include/chatfs.h
@@ -11,7 +11,22 @@ namespace chatfs
     #define CHATFSERR(ERR) -ERR
     using s_stat = struct stat;
     using fuse_op = struct fuse_operations;
+    using fuse_fi = struct fuse_file_info;
+    /**
+     * @brief Get attributes of file
+     * @param p Path to file
+     * @param st Stat struct to fill
+    */
     int chatfs_get_attr(const char *p, s_stat *st);
+    /**
+     * @brief Read directory
+     * @param p Path to directory
+     * @param b Buffer to fill
+     * @param filler Fuse filler function to fill buffer
+     * @param offset Offsets of directory entries
+     * @param fi Fuse file info
+    */
+    int chatfs_read_dir(const char *p, void *b, fuse_fill_dir_t filler, off_t offset,  *fi);
     extern fuse_op chatfs_operations;
 } // namespace chatfs
 
